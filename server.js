2d735282490bbd1e0588ef0d44eff8ae99cd7e80
akulsr0 <akulsr0@gmail.com>
2020-05-19 13:29:13 +0530

    added search route to server

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use('/', require('./routes/index'));
 app.use('/programs', require('./routes/programs'));
 app.use('/questions', require('./routes/questions'));
+app.use('/search', require('./routes/search'));
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
