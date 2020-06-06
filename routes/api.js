4707564d482c5a04dee9a283f2e8af185264c773
akulsr0 <akulsr0@gmail.com>
2020-06-06 13:18:18 +0530

    added topics to api

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,7 @@ var fs = Promise.promisifyAll(require('fs'));
 const path = require('path');
 const hljs = require('highlight.js');
 const { questions } = require('../utils/gto');
+const topics = require('../utils/topics');
 
 router.get('/gto/all', (req, res) => {
   res.json(questions);
@@ -66,6 +67,60 @@ router.get('/programs/all', async (req, res) => {
       programs.push(program);
     });
     res.json(programs);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get('/topics', async (req, res) => {
+  try {
+    const programsDir = path.join(__dirname, '../programs');
+    const programsFiles = await fs.readdirAsync(programsDir);
+    const programs = [];
+    programsFiles.forEach((file) => {
+      const slug = file.split('.')[0];
+      const title = slug
+        .split('-')
+        .map((w) => w.charAt(0).toUpperCase() + w.slice(1))
+        .join(' ');
+      const searchKey = slug.replace(/-/g, '');
+      const mdContent = fs.readFileSync(programsDir + '/' + file, 'utf8');
+      const program = { slug, title, searchKey, mdContent };
+      programs.push(program);
+    });
+
+    const questionsDir = path.join(__dirname, '../questions');
+    const quesFiles = await fs.readdirAsync(questionsDir);
+    const questions = [];
+    quesFiles.forEach((file) => {
+      const slug = file.split('.')[0];
+      const titleFirstWord = slug.split('-')[0];
+      const title =
+        titleFirstWord.charAt(0).toUpperCase() +
+        titleFirstWord.slice(1) +
+        ' ' +
+        slug.split('-').slice(1).join(' ');
+      const searchKey = slug.replace(/-/g, '');
+      const mdContent = fs.readFileSync(questionsDir + '/' + file, 'utf8');
+      const question = { slug, title, searchKey, mdContent };
+      questions.push(question);
+    });
+
+    const topicObj = {};
+    topics.sort();
+    for (let topic of topics) {
+      topicObj[topic] = [];
+    }
+    let contentArr = [...programs, ...questions];
+    let topicKeys = Object.keys(topicObj);
+    for (let key of topicKeys) {
+      for (let c of contentArr) {
+        if (c.searchKey.includes(key)) {
+          topicObj[key].push(c);
+        }
+      }
+    }
+    res.json({ topics: topicObj });
   } catch (error) {
     console.log(error);
   }
